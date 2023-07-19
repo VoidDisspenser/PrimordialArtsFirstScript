13a85fb091778861068725d48be3db1254b4b165
VoidDisspenser <137305036+VoidDisspenser@users.noreply.github.com>
2023-07-19 17:01:42 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -37,3 +37,14 @@ end)
 c:Button("Safe-Zone", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(4086.654541015625, 854.4833374023438, -4174.62060546875)
 end)
+
+c:Button("Seal", function()
+local sealC = true
+while sealC do
+local args = {
+    [1] = "UltimateAttackPermit"
+}
+
+game:GetService("Players").LocalPlayer.Character:FindFirstChild("Equinox Seal").RemoteFunction:InvokeServer(unpack(args))
+end
+end)
