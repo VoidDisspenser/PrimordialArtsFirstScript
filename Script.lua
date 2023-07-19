420bda5ca7fd6710d6da7784b90dfdf83655782f
VoidDisspenser <137305036+VoidDisspenser@users.noreply.github.com>
2023-07-19 13:02:46 +0200

    Add files via upload

---
+++ Script.lua
@@ -0,0 +1,35 @@
+local Luxtl = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Luxware-UI-Library/main/Source.lua"))()
+
+local Luxt = Luxtl.CreateWindow("BCO", 6105620301)
+
+local mainTab = Luxt:Tab("MISC", 6087485864)
+
+local g = mainTab:Section("Scripts")
+
+g:Button("Dex Explorer", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))()
+end)
+
+g:Button("Anti-Afk", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/VoidDisspenser/Scripts/main/idk"))()
+end)
+
+g:Button("Time-Stop", function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Time%20Stop%20Obfuscator'))()
+end)
+
+local teleportsTab = Luxt:Tab("Teleports")
+
+local c = teleportsTab:Section("Temple")
+
+c:Button("Teleport-Active", function()
+    local args = {
+        [1] = "Confirm"
+    }
+    
+    workspace.Bases.Base1.objects.khrysosteleporter.RemoteFunction:InvokeServer(unpack(args))
+end)
+
+c:Button("Safe-Zone", function()
+    game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(4086.654541015625, 854.4833374023438, -4174.62060546875)
+end)
