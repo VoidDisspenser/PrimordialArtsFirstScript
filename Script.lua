9ca0b84a29fc37698a3ee3dc0234c3f911ceb398
VoidDisspenser <137305036+VoidDisspenser@users.noreply.github.com>
2023-07-23 21:43:02 +0200

    Update Script.lua

--- Script.lua
+++ Script.lua
@@ -1,70 +1,85 @@
-local Luxtl = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Luxware-UI-Library/main/Source.lua"))()
+local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/TheoTheEpic/AquaLib/main/AquaLib.lua'))()
 
-local Luxt = Luxtl.CreateWindow("BCO", 6105620301)
 
-local mainTab = Luxt:Tab("MISC", 6087485864)
+local window = lib.createWindow("Aqua", "BCO2", true) 
+local tab1 = window.createTab("Main")
+local section1 = tab1.createSection("Scripts", false)
+local section2 = tab1.createSection("Temple", false)
 
-local g = mainTab:Section("Scripts")
-
-g:Button("Dex Explorer", function()
+section1.createButton("Dex", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/infyiff/backup/main/dex.lua"))()
 end)
-
-g:Button("Anti-Afk", function()
+section1.createButton("Anti-Afk", function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/VoidDisspenser/Scripts/main/idk"))()
 end)
-
-g:Button("Infinite Yield", function()
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
-end)
-
-g:Button("Time-Stop", function()
+section1.createButton("TimeStop", function()
     loadstring(game:HttpGet('https://raw.githubusercontent.com/GhostPlayer352/Test4/main/Time%20Stop%20Obfuscator'))()
 end)
 
-g:Button("BCO-SCRIPT", function()
-    loadstring(game:HttpGet(('https://raw.githubusercontent.com/xAI-13/Personal-Scripts/main/BCO'),true))()
-end)
-
-local teleportsTab = Luxt:Tab("Teleports")
-
-local c = teleportsTab:Section("Temple")
-
-c:Button("Teleport-Active", function()
-    local args = {
-        [1] = "Confirm"
-    }
-    
-    workspace.Bases.Base1.objects.khrysosteleporter.RemoteFunction:InvokeServer(unpack(args))
-end)
-
-c:Button("Torment", function()
-   local tormentVote = true
-        while tormentVote do
-            local args = {
-    [1] = "Torment"
-}
+section2.createToggle("Torment-Mode", false, function(value)
+	while value == true do
+        local args = {
+            [1] = "Torment"
+        }
 
 game:GetService("ReplicatedStorage").VoteRemote:InvokeServer(unpack(args))
-        end           
+    end
+end)
+section2.createToggle("Kelp-Vote", false, function(value)
+	while value == true do
+        local args = {
+            [1] = "LegendaryItem"
+        }
+        
+        game:GetService("ReplicatedStorage").WageRemote:InvokeServer(unpack(args))    
+    end
 end)
 
-c:Button("Safe-Zone", function()
+
+tab1.createDropdown("Weapon-Auto", {"Seal", "Star", "None"}, "None", function(value)
+    while value == "Seal" do
+        local args = {
+            [1] = "UltimateAttackPermit"
+            }
+            
+        game:GetService("Players").LocalPlayer.Character:FindFirstChild("Equinox Seal").RemoteFunction:InvokeServer(unpack(args))
+    end
+    while value == "Star" do
+        local args = {
+            [1] = "shoot",
+            [2] = {
+                [1] = Vector3.new(-129.3816680908203, 151.88101196289062, -232.2627716064453),
+                [2] = Vector3.new(162.0973663330078, 8.000038146972656, -112.4929428100586)
+            }
+        }
+        
+        game:GetService("Players").LocalPlayer.Character:FindFirstChild("Void Star").RemoteFunction:InvokeServer(unpack(args))
+        local args = {
+            [1] = "shoot",
+            [2] = {
+                [1] = Vector3.new(-187.27528381347656, 152.1672821044922, -166.65953063964844),
+                [2] = Vector3.new(-170.7216796875, 8.000038146972656, 109.27735137939453)
+            }
+        }
+        
+        game:GetService("Players").LocalPlayer.Character:FindFirstChild("Void Star").RemoteFunction:InvokeServer(unpack(args))
+        local args = {
+            [1] = "shoot",
+            [2] = {
+                [1] = Vector3.new(112.89905548095703, 152.06312561035156, -59.00821304321289),
+                [2] = Vector3.new(-6.393074035644531, 6.000038146972656, -4.472465515136719)
+            }
+        }
+        
+        game:GetService("Players").LocalPlayer.Character:FindFirstChild("Void Star").RemoteFunction:InvokeServer(unpack(args))
+                
+    end
+end)
+
+tab1.createButton("Safe-Zone", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(4086.654541015625, 854.4833374023438, -4174.62060546875)
 end)
-
-c:Button("Unsafe-Zone", function()
+tab1.createButton("Unsafe-Zone", function()
     game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(-1.7520514726638794, 70.96434783935547, -108.06066131591797)
-end)
-
-c:Button("Seal!", function()
-local sealC = true
-    while sealC do
-             local args = {
-    [1] = "UltimateAttackPermit"
-}
-
-game:GetService("Players").LocalPlayer.Character:FindFirstChild("Equinox Seal").RemoteFunction:InvokeServer(unpack(args))
-    end
 end)
 
